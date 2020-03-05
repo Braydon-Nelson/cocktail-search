4da37b01757685d36424508af6b409e643b89f23
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-04 19:14:54 -0700

    added html routes for sign in and sign up along with a signup api post

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,25 @@
 const path = require("path");
+var db = require("../models");
 
 module.exports = function (app) {
+
+    // Sign up API POST --- STILL NEED TO MAKE LOGIN!!!
+    app.post("/api/signup", function (req, res) {
+        db.User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(function () {
+                res.redirect("/api/login");
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+
+
 
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,12 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+    // Sign in page route
+    app.get("/sign-in", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/sign-in.html"));
+    });
+    // Sign up page route
+    app.get("/sign-up", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/sign-up.html"));
+    });
 };
