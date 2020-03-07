3423fd3ecf9d217def78b7a85768b30fd79fa657
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-07 12:03:07 -0700

    got the login part, need to do sign up

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -7,3 +7,49 @@ signUpButton.addEventListener('click', () =>
 
 signInButton.addEventListener('click', () =>
     container.classList.remove('right-panel-active'));
+
+
+$(document).ready(function () {
+    // Getting references to our form and inputs
+    let loginForm = $("form.login");
+    let usernameInput = $("input#username-input");
+    let passwordInput = $("input#password-input");
+
+    // When the form is submitted, we validate there's an username and password entered
+    loginForm.on("submit", function (event) {
+        event.preventDefault();
+        var userData = {
+            username: usernameInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!userData.username || !userData.password) {
+            return;
+        }
+
+        // If we have an username and password we run the loginUser function and clear the form
+        loginUser(userData.username, userData.password);
+        usernameInput.val("");
+        passwordInput.val("");
+    });
+
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(username, password) {
+        $.post("/api/login", {
+            username: username,
+            password: password
+        })
+            .then(function () {
+                window.location.replace("/");
+                // If there's an error, log the error
+                console.log("SOMETHING WORKED");
+
+            })
+            .catch(function (err) {
+                console.log("SOMETHING BROKE");
+
+                console.log(err);
+            });
+
+    }
+});
