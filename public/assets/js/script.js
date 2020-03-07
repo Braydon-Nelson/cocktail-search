401326f7de5935a0f441140145cecdde5b6a1f6f
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-07 12:53:47 -0700

    fixed script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,9 +3,5 @@ $(document).ready(function () {
     $(function () {
         $('[data-toggle="popover"]').popover()
     })
-<<<<<<< HEAD
-
-=======
->>>>>>> 252bcc0109c1145b23949d621cca5db5d4b9d3f4
 });
 
