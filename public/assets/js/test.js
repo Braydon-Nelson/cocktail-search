66f29669de25029abd1853ecf3504e587e5fc4c3
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-12 20:19:58 -0600

    got the favorite img to change on click

---
+++ public/assets/js/test.js
@@ -0,0 +1,17 @@
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
+
+
+$(".favoriteDrink").on("click", function (event) {
+    console.log(this.children[0]);
+
+    $(this.children[0]).attr("src", "assets/images/png/001-martini.png");
+})
+
+
+
+
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
+// THIS FILE SHOULD BE DELETED FOR PRODUCTION
