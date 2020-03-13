0b2fa30330ccc8eb11f9b466415f367df868d785
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-12 21:46:34 -0600

    added jaws

--- config/config.js
+++ config/config.js
@@ -21,6 +21,7 @@ module.exports = {
     database: "cocktails_production",
     host: "127.0.0.1",
     dialect: "mysql",
-    operatorsAliases: false
+    operatorsAliases: false,
+    "use_env_variable": "JAWSDB_URL"
   }
 };
