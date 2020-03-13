ea096ec86805494c8141afb4f17429f14a85f7a4
Braydon Nelson <braydon.nelson@outlook.com>
2020-03-12 21:49:49 -0600

    fixed jaws

--- config/config.js
+++ config/config.js
@@ -16,12 +16,8 @@ module.exports = {
     operatorsAliases: false
   },
   production: {
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: "cocktails_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false,
-    "use_env_variable": "JAWSDB_URL"
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+
   }
 };
