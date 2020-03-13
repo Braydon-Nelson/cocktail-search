cde125861dd178da30c6063e28e91f87bb0253a9
taypal <tay.houk@yahoo.com>
2020-03-12 18:37:51 -0600

    map

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -35,45 +35,115 @@ $(document).ready(function () {
 
     document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
+    //google map
+    // function initAutocomplete() {
+    //     var map = new google.maps.Map(document.getElementById('map'), {
+    //         center: { lat: -33.8688, lng: 151.2195 },
+    //         zoom: 13,
+    //         mapTypeId: 'roadmap'
+    //     });
+
+    //     // Create the search box and link it to the UI element.
+    //     var input = document.getElementById('pac-input');
+    //     var searchBox = new google.maps.places.SearchBox(input);
+    //     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    //     // Bias the SearchBox results towards current map's viewport.
+    //     map.addListener('bounds_changed', function () {
+    //         searchBox.setBounds(map.getBounds());
+    //     });
+
+    //     var markers = [];
+    //     // Listen for the event fired when the user selects a prediction and retrieve
+    //     // more details for that place.
+    //     searchBox.addListener('places_changed', function () {
+    //         var places = searchBox.getPlaces();
+    //         if (places.length == 0) {
+    //             return;
+    //         }
+
+    //         // Clear out the old markers.
+    //         markers.forEach(function (marker) {
+    //             marker.setMap(null);
+    //         });
+
+    //         markers = [];
+
+    //         // For each place, get the icon, name and location.
+    //         var bounds = new google.maps.LatLngBounds();
+    //         places.forEach(function (place) {
+    //             if (!place.geometry) {
+    //                 console.log("Returned place contains no geometry");
+    //                 return;
+    //             }
+    //             var icon = {
+    //                 url: place.icon,
+    //                 size: new google.maps.Size(71, 71),
+    //                 origin: new google.maps.Point(0, 0),
+    //                 anchor: new google.maps.Point(17, 34),
+    //                 scaledSize: new google.maps.Size(25, 25)
+    //             };
+
+    //             // Create a marker for each place.
+    //             markers.push(new google.maps.Marker({
+    //                 map: map,
+    //                 icon: icon,
+    //                 title: place.name,
+    //                 position: place.geometry.location
+    //             }));
+
+    //             if (place.geometry.viewport) {
+    //                 // Only geocodes have viewport.
+    //                 bounds.union(place.geometry.viewport);
+    //             } else {
+    //                 bounds.extend(place.geometry.location);
+    //             }
+    //         });
+    //         map.fitBounds(bounds);
+    //     });
+    // }
+
+
+
     //Google Maps API
-    var map;
-    var service;
-    var infowindow;
-    function initialize() {
-        var sandy = new google.maps.LatLng(40.586680, -111.895080);
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: sandy,
-            zoom: 15
-        });
-        var request = {
-            location: sandy,
-            radius: '500',
-            type: ['liquor store']
-        };
-        service = new google.maps.places.PlacesService(map);
+    // var map;
+    // var service;
+    // var infowindow;
+    // function initialize() {
+    //     var sandy = new google.maps.LatLng(40.586680, -111.895080);
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //         center: sandy,
+    //         zoom: 15
+    //     });
+    //     var request = {
+    //         location: sandy,
+    //         radius: '500',
+    //         type: ['liquor store']
+    //     };
+    //     service = new google.maps.places.PlacesService(map);
 
-        service.nearbySearch(request, callback);
+    //     service.nearbySearch(request, callback);
 
-        service.textSearch(request, callback);
+    //     service.textSearch(request, callback);
 
-    }
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-        }
-    }
+    // }
+    // function callback(results, status) {
+    //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //         for (var i = 0; i < results.length; i++) {
+    //             createMarker(results[i]);
+    //         }
+    //     }
+    // }
 
-    var settings = {
-        "url": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBFK5Zw23f4gfIGvnMwCVk5seAXh6whHW8&libraries=places",
-        "method": "GET",
-        "timeout": 0,
-    };
+    // var settings = {
+    //     "url": "https://maps.googleapis.com/maps/api/js?key=AIzaSyBFK5Zw23f4gfIGvnMwCVk5seAXh6whHW8&libraries=places",
+    //     "method": "GET",
+    //     "timeout": 0,
+    // };
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
 
 
 });
